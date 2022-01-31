535445e9cb0ef11d046bccc595bdda4976c0bd80
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-31 20:56:14 -0300

    feat: Task Advanced finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputTask, setInputTask] = useState({ description: '' });
+  const [tasks, setTasks] = useState([]);
+  const [id, setId] = useState(0);
+
+  const createTaskHandler = (text) => {
+    setInputTask({ id, description: text, complete: false });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setId(id + 1);
+    setTasks([...tasks, inputTask]);
+  };
+
+  const deleteHandler = (id) => {
+    // filter
+    const newArray = tasks.filter((task) => task.id !== id);
+    setTasks(newArray);
+  };
+
+  const completeHandler = (id) => {
+    // map
+    const newArray = tasks.map((task) => {
+      if (task.id === id) {
+        task.complete = !task.complete;
+      }
+      return task;
+    });
+    setTasks(newArray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Task Advanced</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type='text'
+          placeholder='Ingrese la tarea'
+          value={inputTask.description}
+          onChange={(e) => createTaskHandler(e.target.value)}
+        />
+        <button type='submit'>Agregar Tarea</button>
+      </form>
+      <div className='tasks-container'>
+        {tasks.map((item, index) => (
+          <div key={index} className='task-card'>
+            <button
+              id='delete'
+              type='button'
+              onClick={() => deleteHandler(item.id)}>
+              Delete
+            </button>
+            <p className={item.complete ? 'task-complete' : ''}>
+              {item.description}
+            </p>
+            <button
+              className={item.complete ? 'btn-incomplete' : 'btn-complete'}
+              type='button'
+              onClick={() => completeHandler(item.id)}>
+              {item.complete ? 'Incomplete' : 'Completar'}
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
